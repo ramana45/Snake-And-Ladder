368faf9c7a6926c1e5dbc86c404b2da0d2c37807
ramana <ramanarajupyla@gmail.com>
2020-11-18 14:42:13 +0530

    Adding testcase 2 file

--- src/com/example/SnakeAndLadder.java
+++ src/com/example/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
 		System.out.println("Snake And Ladder Problem");
 		int pos = 0;
 		System.out.println("player position : "+pos);
+		int die = ((int) (6*Math.random())) + 1;
+		System.out.println("die: "+die);
 	}
 }
