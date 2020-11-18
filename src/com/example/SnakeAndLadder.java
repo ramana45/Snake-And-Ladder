8eb263f361eec0ce96ff77e38cc46e212fffa8cb
ramana <ramanarajupyla@gmail.com>
2020-11-18 14:29:31 +0530

    Adding testcase 1 file

--- src/com/example/SnakeAndLadder.java
+++ src/com/example/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.example;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Snake And Ladder Problem");
+		int pos = 0;
+		System.out.println("player position : "+pos);
 	}
 }
